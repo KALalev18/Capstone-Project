3c82e82661a6285b9683be21c065ca84d8ed5a14
cristian20021 <116003823+cristian20021@users.noreply.github.com>
2025-03-08 13:35:20 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -32,3 +32,31 @@ dropzoneContainer.addEventListener('drop', (e) => {
     })
   }
 })
+
+function displayInputField(){
+
+    const inputField = document.querySelector('.input-field');
+    console.log("Yo");
+    inputField.classList.add("show");
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const button3 = document.querySelector('.aurora-glow-button3');
+    if (button3) button3.addEventListener('click', displayInputField);
+
+});
+
+// Upload Script
+
+document.getElementById('file-label').addEventListener('click', () => {
+    const fileInput = document.getElementById('code-upload');
+    fileInput.click();
+});
+
+function handleFileChange(event) {
+    const file = event.target.files[0];
+    if (file) {
+        console.log('File selected:', file.name);
+    }
+}
